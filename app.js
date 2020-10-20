3b7e853fc80969bf29beb231b8c3c30c0bd405b8
Leslie Villatoro <levill98@gmail.com>
2020-10-19 18:05:44 -0700

    added npm installs

---
+++ app.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const { printTable } = require('console-table-printer');
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "rootroot",
+    database: "employees_db"
+  });
